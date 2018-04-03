de68547a601447d76c7310b68ee58494761adc9e
Hebe Hilhorst <hebehh@gmail.com>
2018-04-03 22:32:32 +0000

    Added files

---
+++ server.py
@@ -0,0 +1,53 @@
+import servo
+import httplib
+import json
+import wiringpi
+
+device_name = "pee"
+
+keynames = ["Lights","Camera","Slider"]
+types = ["SWITCH","SWITCH","SLIDER"]
+servopins = [0,1,2]
+
+onoffconstants = ["Off","On"]
+
+def get_server_data():
+    server = "192.241.140.108"
+    port = "5000"
+    headers = {'Content-type': 'application/json'}
+    server_path = "/get"
+
+    connection = httplib.HTTPConnection(server, port=port)
+    connection.request('GET', server_path, headers=headers)
+
+    response = connection.getresponse().read()
+
+    d = json.loads(response.decode())
+
+    return d['data']
+
+def step():
+    data = get_server_data()
+
+    for i in range(len(keynames)):
+        full_key = device_name+"_"+keynames[i]
+        if full_key in data:
+            print full_key
+            if types[i] == "SWITCH":
+                if data[full_key]['value']==onoffconstants[0]:
+                    print "\tUp"
+                    servo.up(servopins[i])
+                elif data[full_key]['value']==onoffconstants[1]:
+                    print "\tDown"
+                    servo.down(servopins[i])
+
+def poll(interval):
+    while True:
+        try:
+            step()
+        except:
+            print "Exception - investigate..."
+            pass
+        wiringpi.delay(interval)
+
+poll(10)
---
+++ servo.py
@@ -0,0 +1,96 @@
+import wiringpi
+import json
+import urllib2
+import time
+
+wiringpi.wiringPiSetupGpio()
+wiringpi.pinMode(2, 1)
+wiringpi.softPwmCreate(2,0,100)
+
+start_time = time.time()
+
+pins = [2,3,4,17,27,22,11,10,9]
+
+def down(pin):
+	wiringpi.softPwmWrite(pins[pin], 24)
+
+def clear():
+	for i in xrange(0, 9): control(i, 0)
+
+def up(pin):
+	wiringpi.softPwmWrite(pins[pin], 19)
+
+def init():
+	for pin in pins:
+		wiringpi.pinMode(pin, 1)
+		wiringpi.softPwmCreate(pin,0,100)
+
+init()
+
+def pulse(pin, interval):
+	# print "Pulsing... start_time = %f" % start_time
+
+	if interval == 0:
+		# print "Resetting..."
+		control(pin, 0)
+
+	elif (int((time.time() - start_time)/interval) % 2):
+		down(pin)
+	else:
+		up(pin)
+
+def control(pin, val):
+	wiringpi.softPwmWrite(pins[pin], val)
+
+def reset():
+	for i in xrange(0,3):
+		up(i)
+	wiringpi.delay(700)
+	for i in xrange(3,6):
+		up(i)
+	wiringpi.delay(700)
+	for i in xrange(6,9):
+		up(i)
+
+def redown():
+        for i in xrange(0,3):
+                down(i)
+        wiringpi.delay(700)
+        for i in xrange(3,6):
+                down(i)
+        wiringpi.delay(700)
+        for i in xrange(6,9):
+                down(i)
+
+def testdown():
+        for i in xrange(0,9):
+                down(i)
+		wiringpi.delay(300)
+
+def testup():
+        for i in xrange(0,9):
+                up(i)
+                wiringpi.delay(300)
+
+
+def all_move(val):
+	for pin in pins:
+		wiringpi.softPwmWrite(pin, val)
+
+def move(val):
+	wiringpi.softPwmWrite(2,val)
+
+def run(rangelow, rangehigh, inc):
+	for i in xrange(rangelow, rangehigh, inc):
+		wiringpi.softPwmWrite(2, i)
+		wiringpi.delay(100)
+
+def run_pattern(multiplier=1, pattern=None):
+	print "Getting pattern..."
+	if pattern==None:
+		pattern = json.loads(json.loads(urllib2.urlopen('http://dweet.io/get/latest/dweet/for/feelybot').read())['with'][0]['content']['pattern'])
+	print "Got Pattern. running..."
+	for p in pattern:
+		wiringpi.delay(int(p['time']*multiplier))
+		print "Cell %d at %d millis." % (p['cell'],p['time'])
+		down(p['cell'])
